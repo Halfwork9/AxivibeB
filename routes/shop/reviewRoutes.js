5273283aaa937ea38a85b94c33b113bdf06067d1
Halfwork9 <halfworks9@gmail.com>
2025-10-09 13:35:15 +0530

    Create reviewRoutes.js

---
+++ routes/shop/reviewRoutes.js
@@ -0,0 +1,11 @@
+// routes/shop/reviewRoutes.js
+import express from "express";
+import { addReview } from "../../controllers/shop/review-controller.js";
+import { authMiddleware } from "../../middlewares/auth-middleware.js"; // Ensure user is logged in
+
+const router = express.Router();
+
+// POST /api/shop/products/:productId/reviews
+router.post("/products/:productId/reviews", authMiddleware, addReview);
+
+export default router;
