002f56ab9abbae7417bf38797b1367dc05457b48
Halfwork9 <halfworks9@gmail.com>
2025-10-10 12:16:32 +0530

    Delete routes/shop/reviewRoutes.js

--- routes/shop/reviewRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { addReview } from "../../controllers/shop/review-controller.js";
-import { authMiddleware } from "../../controllers/auth/auth-controller.js";
-
-const router = express.Router();
-
-router.post("/:productId/reviews", authMiddleware, addReview);
-
-export default router;
