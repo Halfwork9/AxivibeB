9b0c3d6944ce51993abbef6fc6b71964bbf0d3cb
Halfwork9 <halfworks9@gmail.com>
2025-10-10 13:51:01 +0530

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,23 +1,43 @@
+import mongoose from "mongoose";
+
 const ProductSchema = new mongoose.Schema(
   {
     image: String,
     title: String,
     description: String,
-    brandId: { type: mongoose.Schema.Types.ObjectId, ref: "Brand" },
-    categoryId: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
+    brandId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Brand",
+    },
+    categoryId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+    },
     price: Number,
     salePrice: Number,
     totalStock: Number,
-    averageReview: { type: Number, default: 0 },
+    averageReview: {
+      type: Number,
+      default: 0,
+    },
     reviews: [
       {
-        userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+        userId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        },
         userName: String,
         rating: Number,
         comment: String,
-        createdAt: { type: Date, default: Date.now },
+        createdAt: {
+          type: Date,
+          default: Date.now,
+        },
       },
     ],
   },
   { timestamps: true }
 );
+
+const Product = mongoose.model("Product", ProductSchema);
+export default Product;
