fe6f16252fe79b4624ed750548d4b2fc76208720
Halfwork9 <halfworks9@gmail.com>
2025-10-13 13:41:04 +0530

    Update brand-routes.js

--- routes/admin/brand-routes.js
+++ routes/admin/brand-routes.js
@@ -1,18 +1,13 @@
 import express from "express";
 import multer from "multer";
-import {
-  createBrand,
-  deleteBrand,
-  editBrand,
-  getAllBrands,
-} from "../../controllers/admin/brand-controller.js";
+import { createBrand, deleteBrand, getAllBrands } from "../../controllers/admin/brand-controller.js";
 
 const router = express.Router();
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
+// ✅ Field name MUST be "logo"
 router.post("/", upload.single("logo"), createBrand);
-router.put("/:id", upload.single("logo"), editBrand);
 router.get("/", getAllBrands);
 router.delete("/:id", deleteBrand);
 
