0fb5cbda692e50f8181bb52875bbd16c2fd5fcb0
Halfwork9 <halfworks9@gmail.com>
2025-10-17 12:56:01 +0530

    Update products-controller.js

--- controllers/admin/products-controller.js
+++ controllers/admin/products-controller.js
@@ -1,19 +1,5 @@
 import Product from "../../models/Product.js";
-import { imageUploadUtil } from "../../helpers/cloudinary.js";
 
-// Upload image
-export const handleImageUpload = async (req, res) => {
-  try {
-    const b64 = Buffer.from(req.file.buffer).toString("base64");
-    const url = "data:" + req.file.mimetype + ";base64," + b64;
-    const result = await imageUploadUtil(url);
-
-    res.json({ success: true, result });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ success: false, message: "Error occurred" });
-  }
-};
 
 // ADD product
 // ADD product
