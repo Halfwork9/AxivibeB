3eaad1a378c568f2d5e24ec04cd99e4c04ba2a17
Halfwork9 <halfworks9@gmail.com>
2025-10-24 11:47:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -34,7 +34,19 @@ const allowedOrigins = [
   "https://axivibe1.onrender.com",
   "https://axivibe.netlify.app",
   "https://nikhilmamdekar.site",
+   "https://accounts.google.com",
 ];
+
+const corsOptions = {
+  origin: allowedOrigins,
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
+
 app.use(cors({
   origin: function (origin, callback) {
     // Allow requests with no origin (like mobile apps, curl, etc.)
