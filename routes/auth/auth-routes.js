c87f4df82e8b2a4c9379c24ebda6bb36c8818cb9
Halfwork9 <halfworks9@gmail.com>
2025-10-25 13:52:37 +0530

    Update auth-routes.js

--- routes/auth/auth-routes.js
+++ routes/auth/auth-routes.js
@@ -24,8 +24,17 @@ router.post("/forgot-password", forgotPassword);
 router.post("/reset-password/:token", resetPassword);
 
 // Auth Check
+// --- CHECK AUTH ROUTE ---
 router.get("/check-auth", authMiddleware, (req, res) => {
-  res.status(200).json({ success: true, user: req.user });
+  if (!req.user) {
+    return res.status(401).json({ success: false, message: "Not authenticated" });
+  }
+
+  res.status(200).json({
+    success: true,
+    message: "Authenticated user",
+    user: req.user,
+  });
 });
 
 export default router;
