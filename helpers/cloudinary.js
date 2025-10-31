896c12e2851a8438fb11380ea0262e3fbd6f15c2
Halfwork9 <halfworks9@gmail.com>
2025-10-31 12:24:34 +0530

    Update cloudinary.js

--- helpers/cloudinary.js
+++ helpers/cloudinary.js
@@ -9,6 +9,8 @@ cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
+  timeout: 30000,
 });
 
 // Export the configured instance to be used throughout your backend
