c53ec58a0b375f324af34b30726c90628cf06fb5
Halfwork9 <halfworks9@gmail.com>
2025-11-01 12:23:12 +0530

    Update order-routes.js

--- routes/admin/order-routes.js
+++ routes/admin/order-routes.js
@@ -3,6 +3,7 @@ import {
   getAllOrdersOfAllUsers,
   getOrderDetailsForAdmin,
   updateOrderStatus,
+   updatePaymentStatus,
 } from "../../controllers/admin/order-controller.js";
 import {
   getOrderStats,
@@ -18,5 +19,5 @@ router.put("/update/:id", updateOrderStatus);
 // 📊 New Analytics Routes
 router.get("/stats", getOrderStats);
 router.get("/sales-overview", getSalesOverview);
-
+router.put("/:id/payment-status", updatePaymentStatus);
 export default router;
