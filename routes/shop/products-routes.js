2c750c566c36c2a1b2a7874341880240b4db742b
Halfwork9 <halfworks9@gmail.com>
2025-11-13 17:12:22 +0530

    Update products-routes.js

--- routes/shop/products-routes.js
+++ routes/shop/products-routes.js
@@ -5,6 +5,7 @@ import {
 } from "../../controllers/shop/products-controller.js";
 import { addReview } from "../../controllers/shop/review-controller.js";
 import { authMiddleware } from "../../controllers/auth/auth-controller.js";
+import ProductCache from "../../models/ProductCache.js";
 
 const router = express.Router();
 
@@ -16,5 +17,10 @@ router.get("/product-details/:id", getProductById);
 
 // ⭐ Add product review
 router.post("/:productId/reviews", authMiddleware, addReview);
+
+router.delete("/clear-cache", async (req, res) => {
+  await ProductCache.deleteMany({});
+  res.json({ success: true, message: "Product cache cleared" });
+});
 
 export default router;
