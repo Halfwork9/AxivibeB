6200bc5a24b8ee179a3d6bab5501ba2fc9c4bda2
Halfwork9 <halfworks9@gmail.com>
2025-11-13 17:11:45 +0530

    Update products-routes.js

--- routes/admin/products-routes.js
+++ routes/admin/products-routes.js
@@ -5,6 +5,7 @@ import {
   fetchAllProducts,
   deleteProduct,
 } from "../../controllers/admin/products-controller.js";
+import ProductCache from "../../models/ProductCache.js";
 
 const router = express.Router();
 
@@ -13,5 +14,9 @@ router.post("/add", addProduct);
 router.put("/edit/:id", editProduct);
 router.delete("/delete/:id", deleteProduct);
 router.get("/get", fetchAllProducts);
+router.delete("/clear-cache", async (req, res) => {
+  await ProductCache.deleteMany({});
+  res.json({ success: true, message: "Product cache cleared" });
+});
 
 export default router;
